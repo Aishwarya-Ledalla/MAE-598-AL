53d74d8b405288e97309379209951452cb1904d5
Aishwarya-Ledalla <89552747+Aishwarya-Ledalla@users.noreply.github.com>
2021-12-12 04:39:30 -0700

    Final Update

--- MAE598_Project3_Aishwarya_Ledalla.m
+++ MAE598_Project3_Aishwarya_Ledalla.m
@@ -1,31 +1,39 @@
 %% MAE 598: Project 3 - Topology Optimization
 % Aishwarya Ledalla
 
+
+%% Topology Optimization of a Random Structure
+clear;
+clc;
+
+top88(10,10,0.5,3,2,10)
+
 %%%% Modified by Max Yi Ren (ASU) %%%%%%%%%%%%%%%%%%%%%%%
 %%%% AN 88 LINE TOPOLOGY OPTIMIZATION CODE Nov, 2010 %%%%
 
 function top88(nelx,nely,volfrac,penal,rmin,ft)
-%% MATERIAL PROPERTIES
-E0 = 1;
+%% MATERIAL PROPERTIES: 6061-T6 Aluminum
+E0 = 68.9; % GPa
 Emin = 1e-9;
-nu = 0.3;
+nu = 0.33;
 
 %% PREPARE FINITE ELEMENT ANALYSIS
 A11 = [12 3 -6 -3; 3 12 3 0; -6 3 12 -3; -3 0 -3 12];
 A12 = [-6 -3 0 3; -3 -6 -3 -6; 0 -3 -6 3; 3 -6 3 -6];
 B11 = [-4 3 -2 9; 3 -4 -9 4; -2 -9 -4 -3; 9 4 -3 -4];
 B12 = [ 2 -3 4 -9; -3 2 9 -2; 4 9 2 3; -9 -2 3 2];
-KE = 1/(1-nu^2)/24*([A11 A12;A12’ A11]+nu*[B11 B12;B12’ B11]);
+
+KE = 1/(1-nu^2)/24*([A11 A12;A12 A11]+nu*[B11 B12;B12 B11]);
 nodenrs = reshape(1:(1+nelx)*(1+nely),1+nely,1+nelx);
 edofVec = reshape(2*nodenrs(1:end-1,1:end-1)+1,nelx*nely,1);
 edofMat = repmat(edofVec,1,8)+repmat([0 1 2*nely+[2 3 0 1] -2 -1],nelx*nely,1);
-iK = reshape(kron(edofMat,ones(8,1))’,64*nelx*nely,1);
-jK = reshape(kron(edofMat,ones(1,8))’,64*nelx*nely,1);
+iK = reshape(kron(edofMat,ones(8,1)),64*nelx*nely,1);
+jK = reshape(kron(edofMat,ones(1,8)),64*nelx*nely,1);
 
-% DEFINE LOADS AND SUPPORTS (HALF MBB-BEAM)
-F = sparse(2,1,-1,2*(nely+1)*(nelx+1),1);
+%% DEFINE LOADS AND SUPPORTS (HALF MBB-BEAM)
+F = sparse(3,1,-1,3*(nely+1)*(nelx+1),1);
 U = zeros(2*(nely+1)*(nelx+1),1);
-fixeddofs = union([1:2:2*(nely+1)],[2*(nelx+1)*(nely+1)]);
+fixeddofs = union([1:2:2*(nely+3)],[2*(nelx+1)*(nely+3)]);
 alldofs = [1:2*(nely+1)*(nelx+1)];
 freedofs = setdiff(alldofs,fixeddofs);
 
@@ -34,6 +42,7 @@ iH = ones(nelx*nely*(2*(ceil(rmin)-1)+1)^2,1);
 jH = ones(size(iH));
 sH = zeros(size(iH));
 k = 0;
+
 for i1 = 1:nelx
     for j1 = 1:nely
 
@@ -63,13 +72,13 @@ while change > 0.01
     loop = loop + 1;
 
     %% FE-ANALYSIS
-    K = ?;
-    U(freedofs) = ?;
+    K = (KE')*(Emin + (xPhys^penal)*(E0 - Emin));
+    U(freedofs) = linsolve(K,F(freedofs));
 
     %% OBJECTIVE FUNCTION AND SENSITIVITY ANALYSIS
-    ce = ?; % element-wise strain energy
-    c = ?; % total strain energy
-    dc = ?; % design sensitivity
+    ce = sum(( dot(U(:),KE) * U(:) )); % element-wise strain energy
+    c = sum( (Emin + (xPhys^penal)*(Emax-Emin))*ce ) ; % total strain energy
+    dc = (-penal * (xPhys^(penal-1))*(Emax-Emin))*ce ; % design sensitivity
     dv = ones(nely,nelx);
 
     %% FILTERING/MODIFICATION OF SENSITIVITIES
@@ -81,21 +90,44 @@ while change > 0.01
     end
 
     %% OPTIMALITY CRITERIA UPDATE OF DESIGN VARIABLES AND PHYSICAL DENSITIES
-    while ?
-        ???
+    [xnew,g] = oc(nelx,nely,x,volfrac,dc,dv,g);
+
+    if ft == 0
+        xPhys = x;
+    elseif ft == 1
+        xPhys = H*x'/Hs;
     end
 
     change = max(abs(xnew(:)-x(:)));
     x = xnew;
 
     %% PRINT RESULTS
-    fprintf(’ It.:%5i Obj.:%11.4f Vol.:%7.3f ch.:%7.3f\n’,loop,c, ...
-    mean(xPhys(:)),change);
+    fprintf('It.:%5i Obj.:%11.4f Vol.:%7.3f ch.:%7.3f\n',loop,c, mean(xPhys(:)),change);
 
     %% PLOT DENSITIES
     colormap(gray); imagesc(1-xPhys); caxis([0 1]); axis equal; axis off; drawnow;
 end
-%
+
+%% Optimality criterion
+    function [xnew, g] = oc(nelx,nely,x,volfrac,dc,dv,g)
+    l1 = 0;
+    l2 = 1e9;
+    move = 0.2;
+    % reshape to perform vector operations
+    xnew = zeros(nelx,nely);
+    while (l2-l1)/(l1+l2) > 1e-3
+        lmid = 0.5*(l2+l1);
+        xnew = max(0, max(x - move, min(1, min(x + move, x*sqrt(-dc/dv/lmid)))));
+        gt = g + sum(dv*(xnew-x));
+        if gt > 0
+            l1 = lmid;
+        else
+            l2 = lmid;
+        end
+    end
+    end
+end
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % This Matlab code was written by E. Andreassen, A. Clausen, M. Schevenels,%
 % B. S. Lazarov and O. Sigmund, Department of Solid Mechanics, %
